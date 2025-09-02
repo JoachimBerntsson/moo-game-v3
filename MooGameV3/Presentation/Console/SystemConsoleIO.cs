d689c71ec122c30d2566d6e14944863ed709f317
JoachimBerntsson <joachim.berntsson@gmail.com>
2025-09-02 10:26:37 +0200

    feat(console): add SystemConsoleIO for console I/O operations
    This commit introduces a new class `SystemConsoleIO` in the `MooGameV3.Presentation.Console` namespace. The class implements the `IGameIO` interface and provides methods for reading and writing text to the console. A new method `WriteLineColored` is also added, enabling text output in a specified color while preserving the original console text color.

---
+++ MooGameV3/Presentation/Console/SystemConsoleIO.cs
@@ -0,0 +1,19 @@
+﻿using MooGameV3.Application.Abstractions;
+
+namespace MooGameV3.Presentation.Console;
+
+public sealed class SystemConsoleIO : IGameIO
+{
+	public string? ReadLine() => System.Console.ReadLine();
+	public void Write(string text) => System.Console.Write(text);
+	public void WriteLine(string text) => System.Console.WriteLine(text);
+
+
+	public void WriteLineColored(string text, ConsoleColor color)
+	{
+		var old = System.Console.ForegroundColor;
+		System.Console.ForegroundColor = color;
+		System.Console.WriteLine(text);
+		System.Console.ForegroundColor = old;
+	}
+}
