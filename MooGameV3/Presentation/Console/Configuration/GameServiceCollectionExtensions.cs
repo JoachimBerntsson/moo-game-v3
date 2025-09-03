34ea676830b25f7c1509106c74fe9b2ebefaecda
JoachimBerntsson <joachim.berntsson@gmail.com>
2025-09-03 17:00:24 +0200

    feat(services): add GameServiceCollectionExtensions
    Introduce a new class `GameServiceCollectionExtensions` to encapsulate the registration of game-related services and settings in the dependency injection container. This refactor replaces inline service registrations in `Program.cs` with calls to the new extension methods, improving code organization and readability.
    
    BREAKING CHANGE: Service registration methods have been moved to `GameServiceCollectionExtensions`, altering the way services are configured in `Program.cs`.

---
+++ MooGameV3/Presentation/Console/Configuration/GameServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MooGameV3.Application.Abstractions;
+using MooGameV3.Application.Abstractions.Randomness;
+using MooGameV3.Application.Services;
+using MooGameV3.Domain.Game;
+using MooGameV3.Infrastructure.CodeGeneration;
+using MooGameV3.Infrastructure.FileSystem;
+using MooGameV3.Infrastructure.Randomness;
+using MooGameV3.Presentation.Console.Game;
+using MooGameV3.Presentation.Console.Intro;
+using MooGameV3.Presentation.Console.IO;
+using MooGameV3.Presentation.Console.Scoring;
+
+namespace MooGameV3.Presentation.Console.Configuration;
+
+public static class GameServiceCollectionExtensions
+{
+	public static IServiceCollection AddGameServices(this IServiceCollection services, string scoreFilePath)
+	{
+		return services
+			.AddSingleton<IRandomSource, SystemRandomSource>()
+			.AddSingleton<ICodeGenerator, RandomCodeGenerator>()
+			.AddSingleton<ICodeEvaluator, CodeEvaluator>()
+			.AddSingleton<IScoreRepository>(_ => new FileScoreRepository(
+				new FileScoreRepositoryOptions { Path = scoreFilePath }))
+			.AddSingleton<ScoreService>();
+	}
+
+	public static IServiceCollection AddGameSettings(this IServiceCollection services)
+	{
+		var gameSettings = new GameSettings(
+			DefaultCodeLength: 4,
+			AllowDuplicates: false,
+			PracticeMode: false);
+
+		return services
+			.AddSingleton<IGameSettings>(gameSettings)
+			.AddSingleton<IGameRules>(gameSettings)
+			.AddSingleton<IGuessValidator, GuessValidator>();
+	}
+
+	public static IServiceCollection AddConsoleServices(this IServiceCollection services)
+	{
+		return services
+			.AddSingleton<IGameIO, SystemConsoleIO>()
+			.AddSingleton<IWelcomePrinter, WelcomePrinter>()
+			.AddSingleton<IPromptService, PromptService>()
+			.AddSingleton<IOutputFormatter, OutputFormatter>()
+			.AddSingleton<IScorePresenter, ScorePresenter>()
+			.AddSingleton<IRoundRunner, RoundRunner>()
+			.AddSingleton<ConsoleGameRunner>();
+	}
+}
--- MooGameV3/Presentation/Console/Program.cs
+++ MooGameV3/Presentation/Console/Program.cs
@@ -1,15 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using MooGameV3.Application.Abstractions.Randomness;
-using MooGameV3.Application.Abstractions;
-using MooGameV3.Application.Services;
-using MooGameV3.Domain.Game;
-using MooGameV3.Infrastructure.CodeGeneration;
-using MooGameV3.Infrastructure.Randomness;
-using MooGameV3.Infrastructure.FileSystem;
-using MooGameV3.Presentation.Console.Game;
-using MooGameV3.Presentation.Console.Intro;
-using MooGameV3.Presentation.Console.IO;
-using MooGameV3.Presentation.Console.Scoring;
+using MooGameV3.Presentation.Console.Configuration;
 
 namespace MooGameV3.Presentation.Console;
 
@@ -22,27 +12,9 @@ internal static class Program
 		var scoreFile = Path.Combine(AppContext.BaseDirectory, "result.txt");
 
 		var services = new ServiceCollection()
-
-			.AddSingleton<IRandomSource, SystemRandomSource>()
-			.AddSingleton<ICodeGenerator, RandomCodeGenerator>()
-			.AddSingleton<ICodeEvaluator, CodeEvaluator>()
-			.AddSingleton<IScoreRepository>(_ => new FileScoreRepository(
-				new MooGameV3.Infrastructure.FileSystem.FileScoreRepositoryOptions { Path = scoreFile }))
-			.AddSingleton<ScoreService>()
-			.AddSingleton<IGameIO, SystemConsoleIO>()
-			.AddSingleton<IGuessValidator, GuessValidator>()
-			.AddSingleton<IGameRules>(_ => new GameSettings(
-				DefaultCodeLength: 4,
-				AllowDuplicates: false,
-				PracticeMode: false))
-
-			.AddSingleton<IWelcomePrinter, WelcomePrinter>()
-			.AddSingleton<IPromptService, PromptService>()
-			.AddSingleton<IOutputFormatter, OutputFormatter>()
-			.AddSingleton<IScorePresenter, ScorePresenter>()
-			.AddSingleton<IRoundRunner, RoundRunner>()
-
-			.AddSingleton<ConsoleGameRunner>()
+			.AddGameServices(scoreFile)
+			.AddGameSettings()
+			.AddConsoleServices()
 			.BuildServiceProvider();
 
 		services.GetRequiredService<ConsoleGameRunner>().Run();
